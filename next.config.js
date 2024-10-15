4a821e959939728ade96b7e7c2f248929dc562fd
David <david@syscomdigital.ro>
2024-10-15 15:19:44 +0300

    comits

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   trailingSlash: true,
+//   output: 'export',
+//   assetPrefix: './',
+//   reactStrictMode: false,
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   trailingSlash: true,
-  output: 'export',
-  assetPrefix: './',
+  output: "export",
   reactStrictMode: false,
-}
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/bookAnimation" : "",
+  assetPrefix: isProd ? "/bookAnimation/" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
